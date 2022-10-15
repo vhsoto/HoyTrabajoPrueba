cf839f34adcdefbd00d1d19b63b5a356b39e74d7
vhsoto <vhsotoing@gmail.com>
2022-10-15 06:55:22 -0500

    Add: Bank relationship with Provider

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,5 @@
 class Bank < ApplicationRecord
   validates :bank_name, presence: true, length: { maximum: 50, too_long: "%{count} characters is the maximun allowed" }
+
+  has_many :providers, dependent: :destroy
 end
